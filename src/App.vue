e2fb654ac4361c6b0cf0e4c05ea4c458fcc12a21
Jianjun <gaojianjun2001@hotmail.com>
2026-01-19 16:42:35 +0800

    vue basic. learn template synatax. --2026.1.19”

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
 
 //data
 const name = ref('Rechael');
-const herf = ref('https://www.scrimba.com');
+const href = ref('https://www.scrimba.com');
 const disabled = ref(true);
 const attibutes = {
   id: 'big_emoji',
@@ -23,51 +24,14 @@ const blockLeave = () =>{
 </script>
 
 <template>
-  // <header>
-  //   <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-  //   <div class="wrapper">
-  //     <HelloWorld msg="You did it!" />
-  //   </div>
-  // </header>
-
-  // <main>
-  //   <TheWelcome />
-  // </main>
-  <span>xx</span>
-  <h1>Hi, my name is NAME </h1>
-  <a target="_blank">
-    <button>CLick to visit my favourite website! If you can...</button>
+  <span v-bind="attibutes">xx</span>
+  <h1>Hi, my name is {{name}} </h1>
+  <a :href target="_blank">
+    <button :disabled>CLick to visit my favourite website! If you can...</button>
   </a>
-  <h2>I have been coding for 100 years</h2>
-  <h3>Today is 29 February 1993</h3>
+  <h2>
+     {{  coding_years > 0 
+      ? `I have been coding for ${coding_years}  years. `
+       : "I'm a newbie." }}</h2>
+  <h3>Today is {{ getFormattedDate(New Date()) }}</h3>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
