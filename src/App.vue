5c6e8da721d6c02b2cbe2168d3ce18f7cf45592f
Jianjun <gaojianjun2001@hotmail.com>
2026-01-22 16:49:18 +0800

    make some practics before event handling. 01.22

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 import { ref } from 'vue';
+import { reactive, computed } from 'vue'
 
 //data
 const name = ref('Rechael');
@@ -14,13 +15,53 @@ const attibutes = {
 const coding_years = ref(0);
 const getFormattedDate = (date) => {
   const options = {year: 'numeric', month: 'long', day: 'numeric' } ;
-  return date.toLocalDateString('en-Uk', options);
+  // return date.toLocalDateString('en-Uk', options);
+  return date.toLocaleDateString('en-US');
 };
 const username = ref('rechelktyjohnson');
 const event = ref('dblclick');
 const blockLeave = () =>{
   alert("You can't leave!");
 }
+const formattedDateOld = new Date();
+console.log(formattedDateOld);
+// const formattedDateNew = getFormattedDate(formattedDateOld);
+
+// console.log(formattedDateNew);
+
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
+
+// 一个计算属性 ref
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? 'Yes' : 'No'
+})
+
+// const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+// const parentMessage = ref('Parent')
+// const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+// const parentMessage = 'Parent'
+// const items = [
+//   /* ... */
+//   aa,bb,dd
+// ]
+// items.forEach((item, index) => {
+//   // 可以访问外层的 `parentMessage`
+//   // 而 `item` 和 `index` 只在这个作用域可用
+//   console.log(parentMessage, item.message, index)
+// })
+const myObject = reactive({
+  title: 'How to do lists in Vue',
+  author: 'Jane Doe',
+  publishedAt: '2016-04-10'
+})
+
 </script>
 
 <template>
@@ -33,5 +74,20 @@ const blockLeave = () =>{
      {{  coding_years > 0 
       ? `I have been coding for ${coding_years}  years. `
        : "I'm a newbie." }}</h2>
-  <h3>Today is {{ getFormattedDate(New Date()) }}</h3>
+  <!-- <h3>Today is {{ getFormattedDate(new Date(Date.UTC(2012, 11, 20, 3, 0, 0))) }}</h3> -->
+  <h3>Today is {{ new Date().toLocaleDateString('zh-Hans') }}</h3>
+  <p>Has published books:</p>
+  <span>{{ publishedBooksMessage }}</span>
+  <br/>
+  <button @click="awesome = !awesome">Toggle</button>
+  <br />
+
+<h1 v-if="awesome">Vue is awesome!</h1>
+<h1 v-else>Oh no 😢</h1>
+<ul>
+  <li v-for="value in myObject">
+    {{ value }}
+  </li>
+</ul>
+
 </template>
